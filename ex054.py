9d94f754a2298556f5a26affffe285bebd3fbf88
Guilherme <guilherme.figura@icloud.com>
2024-10-26 16:19:25 -0300

    exercício 54

--- ex054.py
+++ ex054.py
@@ -3,13 +3,15 @@ No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já
 
 import datetime
 
+data = datetime.date.today().year
 maioridade = 0
 menoridade = 0
-for c in range(0, 7):
-    year = int(input('Digite o ano de nascimento: '))
-    data = datetime.date.today().year - year
-    if data >= 18:
+for pessoas in range(1, 8):
+    nascimento = int(input(f'Em quem ano a {pessoas}ª pessoa nasceu? '))
+    idade = data - nascimento
+    if idade >= 18:
         maioridade = maioridade + 1
     else:
         menoridade = menoridade + 1
-print(f'Existem {maioridade} de pessoas com idade MAIOR de 18 anos e {menoridade} pessoas com idade MENOR de 18 anos')
+print(f'Existem {maioridade} de pessoas com idade MAIOR de 18 anos.'
+      f'\nE existem {menoridade} pessoas com idade MENOR de 18 anos.')
