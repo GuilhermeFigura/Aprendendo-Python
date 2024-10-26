d1a992b47a263f04957fb60af553084496510e90
Guilherme <guilherme.figura@icloud.com>
2024-10-26 14:12:17 -0300

    exercício 54

--- ex054.py
+++ ex054.py
@@ -1,7 +1,16 @@
 """Crie um programa que leia o ano de nascimento de sete pessoas.
 No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores."""
 
-x = str(input(f'Digite uma frase: ')).strip().upper()
+import datetime
 
-print(f'{x[2:-1:2]}')
+maioridade = 0
+menoridade = 0
 
+for c in range(0, 7):
+    year = int(input('Digite o ano de nascimento: '))
+    data = datetime.date.today().year - year
+    if data >= 18:
+        maioridade = maioridade + 1
+    else:
+        menoridade = menoridade + 1
+print(f'Existem {maioridade} de pessoas com idade MAIOR de 18 anos e {menoridade} pessoas com idade MENOR de 18 anos')
