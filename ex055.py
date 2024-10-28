1fae7e2baef93c25fbb9933e29ebb9cf9064131b
Guilherme <guilherme.figura@icloud.com>
2024-10-27 21:53:57 -0300

    exercício 55

--- ex055.py
+++ ex055.py
@@ -1,15 +1,15 @@
 """Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos."""
 
-# Pega o primeiro peso
-peso = float(input('Digite peso da 1º pessoa em KG: '))
-máximo = peso
-mínimo = peso
-
-# Pega os outros 4 pesos
-for pessoas in range(2, 6):
+máximo = 0
+mínimo = 0
+for pessoas in range(1, 6):
     peso = float(input(f'Digite peso da {pessoas}º pessoa em KG: '))
-    if peso >= máximo:
+    if pessoas == 1:
         máximo = peso
-    if peso <= mínimo:
         mínimo = peso
+    else:
+        if peso > máximo:
+            máximo = peso
+        if peso < mínimo:
+            mínimo = peso
 print(f'O peso máximo é {máximo}KG e o mínimo é {mínimo}KG.')
